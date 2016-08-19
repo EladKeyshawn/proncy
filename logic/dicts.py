6b179643506e7228b4d70222bd8d144e23769fe4
Elad Keyshawn <elad.keyshawn@gmail.com>
2016-08-20 01:27:11 +0300

    mend

--- logic/dicts.py
+++ logic/dicts.py
@@ -1,4 +1,12 @@
 # -*- coding: UTF-8 -*-
 
 
-enLetterToHeb = {'a': 'א', 'b': 'ב', 'd': 'ד', 'f':'פ', }
+enLetterToHeb = {'a': 'א', 'b': 'ב', 'd': 'ד', 'f':'פ', 'j' : "ג'", 'k' : 'ק' , 'l': "ל", 'm': "מ", 'n': "נ",
+                 'o':"ו", 'p': "פ" , 'q': "ק" , 'r': "ר" , 's': "ס", 't': 'ת', 'u': "ו", 'v' : "ו", 'w': "וו", 'x': 'קס', 'z': "ז"}
+
+
+def testDicOutput():
+    print enLetterToHeb['x']
+
+
+testDicOutput()
