5b0a2f7af39faa10f7ef3cb2b4837d4caab9cc18
Elad Keyshawn <elad.keyshawn@gmail.com>
2016-08-20 14:03:49 +0300

    added common syllables combinations TODO: add more combinations

--- logic/dicts.py
+++ logic/dicts.py
@@ -1,12 +1,19 @@
 # -*- coding: UTF-8 -*-
 
 
+ # Useful english letters to hebrew dicts
+
+ # Bold letter dict from en to he
 enLetterToHeb = {'a': 'א', 'b': 'ב', 'd': 'ד', 'f':'פ', 'j' : "ג'", 'k' : 'ק' , 'l': "ל", 'm': "מ", 'n': "נ",
                  'o':"ו", 'p': "פ" , 'q': "ק" , 'r': "ר" , 's': "ס", 't': 'ת', 'u': "ו", 'v' : "ו", 'w': "וו", 'x': 'קס', 'z': "ז"}
 
 
+# Useful combinations
+
+commonSyllablesDict = {'ea':"י", 'igh': "יי", 'th':"ת'" }
+
 def testDicOutput():
-    print enLetterToHeb['x']
+    print commonSyllablesDict['th'] + commonSyllablesDict['ea'] + commonSyllablesDict['igh']
 
 
 testDicOutput()
